5d1caf8d7f496591db4a6272d35dc6f2d03254d3
xhaktmchl <62229967+xhaktmchl@users.noreply.github.com>
2021-08-18 11:27:27 +0900

    [백준 2523 c++ O] 별 찍기 - 13 풀이 업로드

---
+++ BOJ/구현/[백준 2523 c++ O] 별 찍기 - 13.cpp
@@ -0,0 +1,30 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cstring> // memset 헤더
+using namespace std;
+// [백준 2523 c++ O] 별 찍기 - 13
+// 문제: 
+// 접근: 기본구현
+// 풀이: 
+int n;
+int main(){
+	ios::sync_with_stdio(false); // 계산시간 단축 // cin,scanf 같이 쓰면 오류
+	cin.tie(nullptr); cout.tie(nullptr);// 입출력 시간 단축 // 이것을 쓰면 scanf,printf섞어 쓰면 안됨
+
+	cin >> n;
+	for (int i = 1; i <= n; i++) {
+		for (int j = 1; j <= i; j++) {
+			cout << "*";
+		}
+		cout << '\n';
+	}
+	for (int i = 1; i < n; i++) {
+		for (int j = n - i; j >= 1; j--) {
+			cout << "*";
+		}
+		cout << '\n';
+	}
+	return 0;
+}
