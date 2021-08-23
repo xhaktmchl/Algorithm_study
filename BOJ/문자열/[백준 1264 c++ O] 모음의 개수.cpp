b192b18e705e79429f1ee7fc82af0a56e7983278
xhaktmchl <62229967+xhaktmchl@users.noreply.github.com>
2021-08-24 01:39:15 +0900

    [백준 1264 c++ O] 모음의 개수 풀이 업로드

---
+++ BOJ/문자열/[백준 1264 c++ O] 모음의 개수.cpp
@@ -0,0 +1,29 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cstring> // memset 헤더
+#include <string>
+using namespace std;
+// [백준 1264 c++ O] 모음의 개수
+// 문제:
+// 접근:
+// 시간복잡도: 
+// 풀이: 
+string s;
+
+int main() {
+	ios::sync_with_stdio(false); // 계산시간 단축 // cin,scanf 같이 쓰면 오류
+	cin.tie(nullptr); cout.tie(nullptr);// 입출력 시간 단축 // 이것을 쓰면 scanf,printf섞어 쓰면 안됨
+	
+	while (1) {
+		getline(cin, s);
+		if (s  == "#") { break; }
+		int c = 0;
+		for (int i = 0; i < s.size(); i++) {
+			if (s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u') { c++; }
+			if (s[i] == 'A' || s[i] == 'E' || s[i] == 'I' || s[i] == 'O' || s[i] == 'U') { c++; }
+		}cout << c << '\n';
+	}
+    return 0;
+}
