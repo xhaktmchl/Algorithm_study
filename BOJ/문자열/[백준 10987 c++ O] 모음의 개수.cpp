d18804818394ab1bffaae3448778505d0ce0c115
xhaktmchl <62229967+xhaktmchl@users.noreply.github.com>
2021-08-24 01:34:03 +0900

    [백준 10987 c++ O] 모음의 개수 풀이 업로드

---
+++ BOJ/문자열/[백준 10987 c++ O] 모음의 개수.cpp
@@ -0,0 +1,24 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cstring> // memset 헤더
+#include <string>
+using namespace std;
+// [백준 10987 c++ O] 모음의 개수
+// 문제:
+// 접근:
+// 시간복잡도: 
+// 풀이: 
+int main() {
+	ios::sync_with_stdio(false); // 계산시간 단축 // cin,scanf 같이 쓰면 오류
+	cin.tie(nullptr); cout.tie(nullptr);// 입출력 시간 단축 // 이것을 쓰면 scanf,printf섞어 쓰면 안됨
+	
+	string s;
+	int c = 0;
+	cin >> s;
+	for (int i = 0; i < s.size(); i++) {
+		if (s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u') { c++; }
+	}cout << c << '\n';
+    return 0;
+}
