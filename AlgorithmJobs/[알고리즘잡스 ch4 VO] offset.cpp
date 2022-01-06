0a603b275c73d02a2d5dad8cec7e56bd7be9feca
xhaktmchl <xhaktmchl@naver.com>
2022-01-07 01:19:30 +0900

    Update and rename [알고리즘잡스 ch2 VO] offset.cpp to [알고리즘잡스 ch4 VO] offset.cpp

--- AlgorithmJobs/[알고리즘잡스 ch4 VO] offset.cpp
+++ AlgorithmJobs/[알고리즘잡스 ch4 VO] offset.cpp
@@ -3,7 +3,7 @@
 #include <algorithm>
 using namespace std;
 /*
-[알고리즘잡스 ch2 VO] offset
+[알고리즘잡스 ch4 VO] offset
 풀이: 완탐으로 상하좌우 범위 및 크기비교
 시간복잡도: O(5*5)=25
 */
@@ -29,7 +29,7 @@ int main() {
       if(i-1>=0 && a[i-1][j]<=a[i][j]){ ok=false;} // 상
       if(i+1<5 && a[i+1][j]<=a[i][j]){ ok=false;} // 하
       if(j-1>=0 && a[i][j-1]<=a[i][j]){ ok=false;} // 좌
-      if(i+1<5 && a[i][j+1]<=a[i][j]){ ok=false;} // 우
+      if(j+1<5 && a[i][j+1]<=a[i][j]){ ok=false;} // 우
       if(ok== true){ b[i][j]=-1;} 
     }
   }
