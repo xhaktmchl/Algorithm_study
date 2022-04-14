08212f700c00a3a4ecdf6c7221a3bf5a2e1d849a
xhaktmchl <xhaktmchl@naver.com>
2022-04-14 15:01:33 +0900

    Create [세종대 알고리즘 트레이닝 01주차 문제04] 1~5 숫자 중 3개의 수 중복 안됨, 조합 O.cpp

---
+++ Sejong Algorithm Training/[세종대 알고리즘 트레이닝 01주차 문제04] 1~5 숫자 중 3개의 수 중복 안됨, 조합 O.cpp
@@ -0,0 +1,27 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <algorithm>
+#include <string>
+using namespace std;
+/*
+[세종대 알고리즘 트레이닝 01주차 문제04] 1~5 숫자 중 3개의 수 중복 안됨, 조합 O
+문제: 
+접근: 
+풀이: 3완탐 중 반복
+시간복잡도: 
+풀이2: 완탐 재귀
+*/
+int main() {
+	ios::sync_with_stdio(false); // 계산시간 단축 // cin,scanf 같이 쓰면 오류
+	cin.tie(nullptr); cout.tie(nullptr);// 입출력 시간 단축 // 이것을 쓰면 scanf,printf섞어 쓰면 안됨
+	
+	//1~5 숫자 중 3개의 수 중복 안됨, 조합
+	for (int i = 1; i <= 5; i++) {
+		for (int j = i+1; j <= 5; j++) {
+			for (int p = j+1; p <= 5; p++) {
+				cout << i << "," << j << "," << p << '\n';
+			}
+		}
+	}
+	return 0;
+}
