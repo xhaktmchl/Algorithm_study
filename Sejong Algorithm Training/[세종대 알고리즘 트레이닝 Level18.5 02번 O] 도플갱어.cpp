af199624a2ccb3e468f7d2b8236df7db6cad9404
xhaktmchl <xhaktmchl@naver.com>
2022-05-28 11:57:27 +0900

    Create [세종대 알고리즘 트레이닝 Level18.5 02번 O] 도플갱어.cpp

---
+++ Sejong Algorithm Training/[세종대 알고리즘 트레이닝 Level18.5 02번 O] 도플갱어.cpp
@@ -0,0 +1,38 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
+using namespace std;
+/*
+[세종대 알고리즘 트레이닝 Level18.5 02번 O] 도플갱어
+문제:
+접근: 
+풀이: 완탐
+	//1.입력
+	//2.완탐: 각 숫자의 갯수 카운트
+	//3.출력:
+시간복잡도: O(6)=
+*/
+int a[10];
+int cnt[10];
+bool flag = 0;
+
+int main() {
+	ios::sync_with_stdio(false); // 계산시간 단축 // cin,scanf 같이 쓰면 오류
+	cin.tie(nullptr); cout.tie(nullptr);// 입출력 시간 단축 // 이것을 쓰면 scanf,printf섞어 쓰면 안됨
+
+	//1.입력
+	for (int i = 0; i < 6; i++) {
+		cin >> a[i];
+	}
+	//2.완탐: 각 숫자의 갯수 카운트
+	for (int i = 0; i < 6; i++) {
+		cnt[a[i]]++;
+		if (cnt[a[i]] >= 2) { flag = 1; }
+	}
+	//3.출력: 
+	if (flag) { cout << "도플갱어 발견"; }
+	else { cout << "미발견"; }
+	return 0;
+}
