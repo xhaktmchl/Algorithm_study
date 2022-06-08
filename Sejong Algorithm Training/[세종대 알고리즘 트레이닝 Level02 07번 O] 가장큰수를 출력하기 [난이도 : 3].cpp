9536f47f654ac8d84e84801f4ef951b9d68cf15e
xhaktmchl <xhaktmchl@naver.com>
2022-06-08 15:08:00 +0900

    Create [세종대 알고리즘 트레이닝 Level02 07번 O] 가장큰수를 출력하기 [난이도 : 3].cpp

---
+++ Sejong Algorithm Training/[세종대 알고리즘 트레이닝 Level02 07번 O] 가장큰수를 출력하기 [난이도 : 3].cpp
@@ -0,0 +1,22 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <algorithm>
+using namespace std;
+/*
+[세종대 알고리즘 트레이닝 Level02 07번 O] 가장큰수를 출력하기 [난이도 : 3]
+문제:
+접근: 
+풀이: 
+시간복잡도: O()=
+*/
+int a, b;
+
+int main() {
+	ios::sync_with_stdio(false); // 계산시간 단축 // cin,scanf 같이 쓰면 오류
+	cin.tie(nullptr); cout.tie(nullptr);// 입출력 시간 단축 // 이것을 쓰면 scanf,printf섞어 쓰면 안됨
+
+	cin >> a >> b;
+	if (a > b) { cout << "a가b보다크다"; }
+	else { cout << "b가a보다같거나크다"; }
+    return 0;
+}
