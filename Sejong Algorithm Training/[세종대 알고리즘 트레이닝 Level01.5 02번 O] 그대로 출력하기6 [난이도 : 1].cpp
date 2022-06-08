eba190428a37d0a9d096975c87cbcb4b5cf1bd95
xhaktmchl <xhaktmchl@naver.com>
2022-06-08 14:44:06 +0900

    Create [세종대 알고리즘 트레이닝 Level01.5 02번 O] 그대로 출력하기6 [난이도 : 1].cpp

---
+++ Sejong Algorithm Training/[세종대 알고리즘 트레이닝 Level01.5 02번 O] 그대로 출력하기6 [난이도 : 1].cpp
@@ -0,0 +1,19 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <algorithm>
+using namespace std;
+/*
+[세종대 알고리즘 트레이닝 Level01.5 02번 O] 그대로 출력하기6 [난이도 : 1]
+문제:
+접근: 
+풀이: 
+시간복잡도: O()=
+*/
+
+int main() {
+	ios::sync_with_stdio(false); // 계산시간 단축 // cin,scanf 같이 쓰면 오류
+	cin.tie(nullptr); cout.tie(nullptr);// 입출력 시간 단축 // 이것을 쓰면 scanf,printf섞어 쓰면 안됨
+
+	cout << "158% 입니다";
+    return 0;
+}
