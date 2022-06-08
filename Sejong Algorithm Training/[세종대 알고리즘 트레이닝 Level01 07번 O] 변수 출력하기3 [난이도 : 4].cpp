3ee01dbb3dd5735b4e000b1ca55e83183da070f0
xhaktmchl <xhaktmchl@naver.com>
2022-06-08 11:55:53 +0900

    Create [세종대 알고리즘 트레이닝 Level01 07번 O] 변수 출력하기3 [난이도 : 4].cpp

---
+++ Sejong Algorithm Training/[세종대 알고리즘 트레이닝 Level01 07번 O] 변수 출력하기3 [난이도 : 4].cpp
@@ -0,0 +1,21 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <algorithm>
+using namespace std;
+/*
+[세종대 알고리즘 트레이닝 Level01 07번 O] 변수 출력하기3 [난이도 : 4]
+문제:
+접근: 
+풀이: 
+시간복잡도: O()=
+*/
+int t = 5;
+
+int main() {
+	ios::sync_with_stdio(false); // 계산시간 단축 // cin,scanf 같이 쓰면 오류
+	cin.tie(nullptr); cout.tie(nullptr);// 입출력 시간 단축 // 이것을 쓰면 scanf,printf섞어 쓰면 안됨
+
+	cout << "t에서 1씩 증가=" << t << " " << t + 1 << " " << t + 2 << '\n';
+	cout << "t에서 2씩 감소=" << t << " " << t - 2 << " " << t - 4;
+	return 0;
+}
