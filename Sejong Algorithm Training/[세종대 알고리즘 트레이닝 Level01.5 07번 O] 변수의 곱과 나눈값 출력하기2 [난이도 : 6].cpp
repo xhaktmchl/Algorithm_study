3bb96eaa655a103d2124372bef5f2fe7f0bd0c46
xhaktmchl <xhaktmchl@naver.com>
2022-06-08 14:52:12 +0900

    Create [세종대 알고리즘 트레이닝 Level01.5 07번 O] 변수의 곱과 나눈값 출력하기2 [난이도 : 6].cpp

---
+++ Sejong Algorithm Training/[세종대 알고리즘 트레이닝 Level01.5 07번 O] 변수의 곱과 나눈값 출력하기2 [난이도 : 6].cpp
@@ -0,0 +1,21 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <algorithm>
+using namespace std;
+/*
+[세종대 알고리즘 트레이닝 Level01.5 07번 O] 변수의 곱과 나눈값 출력하기2 [난이도 : 6]
+문제:
+접근: 
+풀이: 
+시간복잡도: O()=
+*/
+int a = 10, b = 3;
+
+int main() {
+	ios::sync_with_stdio(false); // 계산시간 단축 // cin,scanf 같이 쓰면 오류
+	cin.tie(nullptr); cout.tie(nullptr);// 입출력 시간 단축 // 이것을 쓰면 scanf,printf섞어 쓰면 안됨
+
+	cout << a << " * " << b << " = " << a * b << '\n';
+	cout << a << " / " << b << " = " << a / b << '\n';
+    return 0;
+}
