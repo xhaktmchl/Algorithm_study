d54c6704fa474c0c9ef2c16121cf768f43866029
xhaktmchl <xhaktmchl@naver.com>
2022-06-09 16:13:55 +0900

    Create [세종대 알고리즘 트레이닝 Level05 12번 O] 모두 더한 값에 따라 함수 호출하기.cpp

---
+++ Sejong Algorithm Training/[세종대 알고리즘 트레이닝 Level05 12번 O] 모두 더한 값에 따라 함수 호출하기.cpp
@@ -0,0 +1,36 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <algorithm>
+using namespace std;
+/*
+[세종대 알고리즘 트레이닝 Level05 12번 O] 모두 더한 값에 따라 함수 호출하기
+문제:
+접근: 
+풀이: 
+시간복잡도: O()=
+*/
+int a[10];
+int sum = 0;
+
+void QTR() {
+	cout << "QTR100%";
+}
+
+void BBQ() {
+	cout << "BBQ100%";
+}
+
+int main() {
+	ios::sync_with_stdio(false); // 계산시간 단축 // cin,scanf 같이 쓰면 오류
+	cin.tie(nullptr); cout.tie(nullptr);// 입출력 시간 단축 // 이것을 쓰면 scanf,printf섞어 쓰면 안됨
+
+	for (int i = 0; i < 3; i++) {
+		cin >> a[i];
+		sum += a[i];
+	}
+	if (sum > 10) {
+		QTR();
+	}
+	else { BBQ(); }
+    return 0;
+}
