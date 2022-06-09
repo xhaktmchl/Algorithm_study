545ecd4730412cbc3dbd6fa9685a7f4b3512aab5
xhaktmchl <xhaktmchl@naver.com>
2022-06-09 13:57:36 +0900

    Create [세종대 알고리즘 트레이닝 Level03 02번 O] 숫자 값 비교하기.cpp

---
+++ Sejong Algorithm Training/[세종대 알고리즘 트레이닝 Level03 02번 O] 숫자 값 비교하기.cpp
@@ -0,0 +1,27 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <algorithm>
+using namespace std;
+/*
+[세종대 알고리즘 트레이닝 Level03 02번 O] 숫자 값 비교하기
+문제:
+접근: 
+풀이: 
+시간복잡도: O()=
+*/
+int a,b;
+
+int main() {
+	ios::sync_with_stdio(false); // 계산시간 단축 // cin,scanf 같이 쓰면 오류
+	cin.tie(nullptr); cout.tie(nullptr);// 입출력 시간 단축 // 이것을 쓰면 scanf,printf섞어 쓰면 안됨
+
+	cin >> a >> b;
+	if (a == b) {
+		cout << "같은숫자";
+	}
+	else if (a > b) {
+		cout << "큰수는 " << a;
+	}
+	else { cout << "큰수는 " << b; }
+    return 0;
+}
