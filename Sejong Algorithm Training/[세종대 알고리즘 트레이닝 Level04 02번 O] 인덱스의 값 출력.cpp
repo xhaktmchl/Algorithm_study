5831ec15627a48e4c05cb3f6a89028c3858f7382
xhaktmchl <xhaktmchl@naver.com>
2022-06-09 14:55:13 +0900

    Create [세종대 알고리즘 트레이닝 Level04 02번 O] 인덱스의 값 출력.cpp

---
+++ Sejong Algorithm Training/[세종대 알고리즘 트레이닝 Level04 02번 O] 인덱스의 값 출력.cpp
@@ -0,0 +1,22 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <algorithm>
+using namespace std;
+/*
+[세종대 알고리즘 트레이닝 Level04 02번 O] 인덱스의 값 출력
+문제:
+접근: 
+풀이: 
+시간복잡도: O()=
+*/
+int a[10] = { 4,1,3,6,9 };
+int n;
+
+int main() {
+	ios::sync_with_stdio(false); // 계산시간 단축 // cin,scanf 같이 쓰면 오류
+	cin.tie(nullptr); cout.tie(nullptr);// 입출력 시간 단축 // 이것을 쓰면 scanf,printf섞어 쓰면 안됨
+
+	cin >> n;
+	cout << n << "번index의값은" << a[n] << "입니다";
+    return 0;
+}
