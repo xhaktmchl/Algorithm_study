aa099d5edc2fd7a0cb0799dc803fd82d5f15f055
xhaktmchl <xhaktmchl@naver.com>
2022-06-11 20:14:59 +0900

    Create [세종대 알고리즘 트레이닝 Level05.5 10번 O] 숫자 1개 입력받고 값을 채우기.cpp

---
+++ Sejong Algorithm Training/[세종대 알고리즘 트레이닝 Level05.5 10번 O] 숫자 1개 입력받고 값을 채우기.cpp
@@ -0,0 +1,23 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <algorithm>
+using namespace std;
+/*
+[세종대 알고리즘 트레이닝 Level05.5 10번 O] 숫자 1개 입력받고 값을 채우기
+문제:
+접근: 
+풀이: 
+시간복잡도: O()=
+*/
+int n;
+
+int main() {
+	ios::sync_with_stdio(false); // 계산시간 단축 // cin,scanf 같이 쓰면 오류
+	cin.tie(nullptr); cout.tie(nullptr);// 입출력 시간 단축 // 이것을 쓰면 scanf,printf섞어 쓰면 안됨
+
+	cin >> n;
+	for (int i = 1; i <= 6; i++) {
+		cout << n * i << " ";
+	}
+    return 0;
+}
