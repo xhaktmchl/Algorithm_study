f8e36a23ff8f1b7238708c5b20ff43cfa3b2fee7
xhaktmchl <xhaktmchl@naver.com>
2022-06-11 19:06:10 +0900

    Create [세종대 알고리즘 트레이닝 Level4.5 08번 O] 순서대로 배열에 값 채우고 출력하기.cpp

---
+++ Sejong Algorithm Training/[세종대 알고리즘 트레이닝 Level4.5 08번 O] 순서대로 배열에 값 채우고 출력하기.cpp
@@ -0,0 +1,27 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <algorithm>
+using namespace std;
+/*
+[세종대 알고리즘 트레이닝 Level4.5 08번 O] 순서대로 배열에 값 채우고 출력하기
+문제:
+접근: 
+풀이: 
+시간복잡도: O()=
+*/
+int n;
+int a[10];
+
+int main() {
+	ios::sync_with_stdio(false); // 계산시간 단축 // cin,scanf 같이 쓰면 오류
+	cin.tie(nullptr); cout.tie(nullptr);// 입출력 시간 단축 // 이것을 쓰면 scanf,printf섞어 쓰면 안됨
+
+	cin >> n;
+	for (int i = 0; i < 6; i++) {
+		a[i] = n++;
+	}
+	for (int i = 0; i < 6; i++) {
+		cout << a[i] << '\n';
+	}
+    return 0;
+}
