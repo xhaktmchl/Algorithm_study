feffe14a618bb342912bd0f95947d8e68abed11a
xhaktmchl <xhaktmchl@naver.com>
2022-06-11 20:54:13 +0900

    Create [세종대 알고리즘 트레이닝 Level06 05번 O] 배열을 n 번 출력하기 [난이도 : 3].cpp

---
+++ Sejong Algorithm Training/[세종대 알고리즘 트레이닝 Level06 05번 O] 배열을 n 번 출력하기 [난이도 : 3].cpp
@@ -0,0 +1,26 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <algorithm>
+using namespace std;
+/*
+[세종대 알고리즘 트레이닝 Level06 05번 O] 배열을 n 번 출력하기 [난이도 : 3]
+문제:
+접근: 
+풀이: 
+시간복잡도: O()=
+*/
+char a[10] = "BTKA";
+int n;
+
+int main() {
+	ios::sync_with_stdio(false); // 계산시간 단축 // cin,scanf 같이 쓰면 오류
+	cin.tie(nullptr); cout.tie(nullptr);// 입출력 시간 단축 // 이것을 쓰면 scanf,printf섞어 쓰면 안됨
+
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < 4; j++) {
+			cout << a[j] << " ";
+		}cout << '\n';
+	}
+    return 0;
+}
