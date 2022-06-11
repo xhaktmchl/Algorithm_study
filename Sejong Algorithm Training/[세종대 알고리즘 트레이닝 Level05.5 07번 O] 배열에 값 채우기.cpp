a930828dfc8dc86a0cacf6f488e7fa723a9c2f87
xhaktmchl <xhaktmchl@naver.com>
2022-06-11 19:42:47 +0900

    Create [세종대 알고리즘 트레이닝 Level05.5 07번 O] 배열에 값 채우기.cpp

---
+++ Sejong Algorithm Training/[세종대 알고리즘 트레이닝 Level05.5 07번 O] 배열에 값 채우기.cpp
@@ -0,0 +1,23 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <algorithm>
+using namespace std;
+/*
+[세종대 알고리즘 트레이닝 Level05.5 07번 O] 배열에 값 채우기
+문제:
+접근: 
+풀이: 
+시간복잡도: O()=
+*/
+int n;
+
+int main() {
+	ios::sync_with_stdio(false); // 계산시간 단축 // cin,scanf 같이 쓰면 오류
+	cin.tie(nullptr); cout.tie(nullptr);// 입출력 시간 단축 // 이것을 쓰면 scanf,printf섞어 쓰면 안됨
+
+	cin >> n;
+	for (int i = n; i >= n - 3; i--) {
+		cout << i << " ";
+	}
+    return 0;
+}
