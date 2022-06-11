bbc065e5960c7343e372b4b7700b3a3b390ed458
xhaktmchl <xhaktmchl@naver.com>
2022-06-11 20:22:51 +0900

    Create [세종대 알고리즘 트레이닝 Level05.5 12번 O] sum값 출력하기.cpp

---
+++ Sejong Algorithm Training/[세종대 알고리즘 트레이닝 Level05.5 12번 O] sum값 출력하기.cpp
@@ -0,0 +1,26 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <algorithm>
+using namespace std;
+/*
+[세종대 알고리즘 트레이닝 Level05.5 12번 O] sum값 출력하기
+문제:
+접근: 
+풀이: 
+시간복잡도: O()=
+*/
+int a[10];
+int sum = 0;
+
+int main() {
+	ios::sync_with_stdio(false); // 계산시간 단축 // cin,scanf 같이 쓰면 오류
+	cin.tie(nullptr); cout.tie(nullptr);// 입출력 시간 단축 // 이것을 쓰면 scanf,printf섞어 쓰면 안됨
+
+	for (int i = 0; i < 5; i++) {
+		cin >> a[i];
+		sum += a[i];
+	}
+	
+	cout << "합은 " << sum << "입니다";
+    return 0;
+}
