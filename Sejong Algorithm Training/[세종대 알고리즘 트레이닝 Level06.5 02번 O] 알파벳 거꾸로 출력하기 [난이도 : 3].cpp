2ed760f69bc666f45cacac0035306957d23d69f0
xhaktmchl <xhaktmchl@naver.com>
2022-06-12 01:28:09 +0900

    Create [세종대 알고리즘 트레이닝 Level06.5 02번 O] 알파벳 거꾸로 출력하기 [난이도 : 3].cpp

---
+++ Sejong Algorithm Training/[세종대 알고리즘 트레이닝 Level06.5 02번 O] 알파벳 거꾸로 출력하기 [난이도 : 3].cpp
@@ -0,0 +1,23 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <algorithm>
+using namespace std;
+/*
+[세종대 알고리즘 트레이닝 Level06.5 02번 O] 알파벳 거꾸로 출력하기 [난이도 : 3]
+문제:
+접근: 
+풀이: 
+시간복잡도: O()=
+*/
+char ch;
+
+int main() {
+	ios::sync_with_stdio(false); // 계산시간 단축 // cin,scanf 같이 쓰면 오류
+	cin.tie(nullptr); cout.tie(nullptr);// 입출력 시간 단축 // 이것을 쓰면 scanf,printf섞어 쓰면 안됨
+
+	cin >> ch;
+	for (char i = ch; i >= 'a'; i--) {
+		cout << i;
+	}
+    return 0;
+}
