a31d8295fd73ce65a6f6861ee3bf4e126a6c82ce
xhaktmchl <xhaktmchl@naver.com>
2022-06-11 16:53:34 +0900

    Create [세종대 알고리즘 트레이닝 Level3.5 04번 O] 점프 해서 출력하기.cpp

---
+++ Sejong Algorithm Training/[세종대 알고리즘 트레이닝 Level3.5 04번 O] 점프 해서 출력하기.cpp
@@ -0,0 +1,24 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <algorithm>
+using namespace std;
+/*
+[세종대 알고리즘 트레이닝 Level3.5 04번 O] 점프 해서 출력하기
+문제:
+접근: 
+풀이: 
+시간복잡도: O()=
+*/
+int n;
+
+int main() {
+	ios::sync_with_stdio(false); // 계산시간 단축 // cin,scanf 같이 쓰면 오류
+	cin.tie(nullptr); cout.tie(nullptr);// 입출력 시간 단축 // 이것을 쓰면 scanf,printf섞어 쓰면 안됨
+
+	cin >> n;
+	n += 2;
+	for (int i = 0; i < 3; i++) {
+		cout << n + i << " ";
+	}
+    return 0;
+}
