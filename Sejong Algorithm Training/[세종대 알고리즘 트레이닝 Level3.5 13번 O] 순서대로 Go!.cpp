5c11194100e4d2827f2beb00fe5db30d54e33b32
xhaktmchl <xhaktmchl@naver.com>
2022-06-11 17:13:34 +0900

    Create [세종대 알고리즘 트레이닝 Level3.5 13번 O] 순서대로 Go!.cpp

---
+++ Sejong Algorithm Training/[세종대 알고리즘 트레이닝 Level3.5 13번 O] 순서대로 Go!.cpp
@@ -0,0 +1,21 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <algorithm>
+using namespace std;
+/*
+[세종대 알고리즘 트레이닝 Level3.5 13번 O] 순서대로 Go!
+문제:
+접근: 
+풀이: 
+시간복잡도: O()=
+*/
+
+int main() {
+	ios::sync_with_stdio(false); // 계산시간 단축 // cin,scanf 같이 쓰면 오류
+	cin.tie(nullptr); cout.tie(nullptr);// 입출력 시간 단축 // 이것을 쓰면 scanf,printf섞어 쓰면 안됨
+
+	for (int i = 1; i <= 5; i++) {
+		cout << i << "번go!!" << '\n';
+	}
+    return 0;
+}
