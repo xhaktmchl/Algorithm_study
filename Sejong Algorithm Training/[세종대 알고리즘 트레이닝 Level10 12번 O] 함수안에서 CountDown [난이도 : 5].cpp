9eb4f08e6729de558a8e039f28cdfdce665c7cbf
xhaktmchl <xhaktmchl@naver.com>
2022-06-12 23:09:32 +0900

    Create [세종대 알고리즘 트레이닝 Level10 12번 O] 함수안에서 CountDown [난이도 : 5].cpp

---
+++ Sejong Algorithm Training/[세종대 알고리즘 트레이닝 Level10 12번 O] 함수안에서 CountDown [난이도 : 5].cpp
@@ -0,0 +1,31 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <algorithm>
+using namespace std;
+/*
+[세종대 알고리즘 트레이닝 Level10 12번 O] 함수안에서 CountDown [난이도 : 5]
+문제:
+접근: 
+풀이: 
+시간복잡도: O()=
+*/
+
+int INPUT() {
+	int a;
+	cin >> a;
+	return a;
+}
+
+void CountDown(int n) {
+	for (int i = n; i >= 1; i--) {
+		cout << i << " ";
+	}
+}
+int main() {
+	ios::sync_with_stdio(false); // 계산시간 단축 // cin,scanf 같이 쓰면 오류
+	cin.tie(nullptr); cout.tie(nullptr);// 입출력 시간 단축 // 이것을 쓰면 scanf,printf섞어 쓰면 안됨
+
+	int num = INPUT();
+	CountDown(num);
+	return 0;
+}
