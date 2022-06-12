341dce60cd7411753d27f95607518d88aba8cdec
xhaktmchl <xhaktmchl@naver.com>
2022-06-12 22:58:02 +0900

    Create [세종대 알고리즘 트레이닝 Level10 08번 O] 핑퐁 미션.cpp

---
+++ Sejong Algorithm Training/[세종대 알고리즘 트레이닝 Level10 08번 O] 핑퐁 미션.cpp
@@ -0,0 +1,26 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <algorithm>
+using namespace std;
+/*
+[세종대 알고리즘 트레이닝 Level10 08번 O] 핑퐁 미션
+문제:
+접근: 
+풀이: 
+시간복잡도: O()=
+*/
+int stone;
+
+int pingpong(int num) {
+	return num + 10;
+}
+
+int main() {
+	ios::sync_with_stdio(false); // 계산시간 단축 // cin,scanf 같이 쓰면 오류
+	cin.tie(nullptr); cout.tie(nullptr);// 입출력 시간 단축 // 이것을 쓰면 scanf,printf섞어 쓰면 안됨
+
+	cin >> stone;
+	int ret =  pingpong(stone);
+	cout << ret;
+	return 0;
+}
