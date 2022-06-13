63913768d78eb9aa1d6ac9a8f430b8678e67a945
xhaktmchl <xhaktmchl@naver.com>
2022-06-13 11:56:27 +0900

    Create [세종대 알고리즘 트레이닝 Level11.5 05번 O] 글자 바꾸어서 출력하기.cpp

---
+++ Sejong Algorithm Training/[세종대 알고리즘 트레이닝 Level11.5 05번 O] 글자 바꾸어서 출력하기.cpp
@@ -0,0 +1,32 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <algorithm>
+using namespace std;
+/*
+[세종대 알고리즘 트레이닝 Level11.5 05번 O] 글자 바꾸어서 출력하기
+문제:
+접근: 
+풀이: 
+시간복잡도: O()=
+*/
+int a[5][5];
+
+
+int main() {
+	ios::sync_with_stdio(false); // 계산시간 단축 // cin,scanf 같이 쓰면 오류
+	cin.tie(nullptr); cout.tie(nullptr);// 입출력 시간 단축 // 이것을 쓰면 scanf,printf섞어 쓰면 안됨
+
+	for (int i = 0; i < 2; i++) {
+		for (int j = 0; j < 3; j++) {
+			cin >> a[i][j];
+		}
+	}
+
+	for (int i = 0; i < 2; i++) {
+		for (int j = 0; j < 3; j++) {
+			if (a[i][j] == 0) { cout << "#"; }
+			else { cout << a[i][j]; }
+		}cout << '\n';
+	}
+	return 0;
+}
