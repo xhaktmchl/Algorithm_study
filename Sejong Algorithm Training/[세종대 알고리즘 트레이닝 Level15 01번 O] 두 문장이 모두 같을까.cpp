8debec0fb1bd64e8399c86b8b741262b3dd5c363
xhaktmchl <xhaktmchl@naver.com>
2022-06-14 15:50:27 +0900

    Create [세종대 알고리즘 트레이닝 Level15 01번 O] 두 문장이 모두 같을까.cpp

---
+++ Sejong Algorithm Training/[세종대 알고리즘 트레이닝 Level15 01번 O] 두 문장이 모두 같을까.cpp
@@ -0,0 +1,23 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <algorithm>
+#include <string>
+using namespace std;
+/*
+[세종대 알고리즘 트레이닝 Level15 01번 O] 두 문장이 모두 같을까
+문제:
+접근: 
+풀이: 
+시간복잡도: O()=
+*/
+string a, b;
+
+int main() {
+	ios::sync_with_stdio(false); // 계산시간 단축 // cin,scanf 같이 쓰면 오류
+	cin.tie(nullptr); cout.tie(nullptr);// 입출력 시간 단축 // 이것을 쓰면 scanf,printf섞어 쓰면 안됨
+
+	cin >> a >> b;
+	if (a != b) { cout << "다름"; }
+	else { cout << "같음"; }
+	return 0;
+}
