c59499560daf1027d0f8820aa7631104786a6b47
xhaktmchl <xhaktmchl@naver.com>
2022-06-14 14:17:05 +0900

    Create [세종대 알고리즘 트레이닝 Level14 05번 O] 와플 발라먹기.cpp

---
+++ Sejong Algorithm Training/[세종대 알고리즘 트레이닝 Level14 05번 O] 와플 발라먹기.cpp
@@ -0,0 +1,34 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <algorithm>
+#include <string>
+using namespace std;
+/*
+[세종대 알고리즘 트레이닝 Level14 05번 O] 와플 발라먹기
+문제:
+접근: 
+풀이: 
+시간복잡도: O()=
+*/
+int a[5][5];
+int sum = 0;
+
+int main() {
+	ios::sync_with_stdio(false); // 계산시간 단축 // cin,scanf 같이 쓰면 오류
+	cin.tie(nullptr); cout.tie(nullptr);// 입출력 시간 단축 // 이것을 쓰면 scanf,printf섞어 쓰면 안됨
+
+	for (int i = 0; i < 3; i++) {
+		for (int j = 0; j< 3; j++) {
+			cin>>a[i][j];
+		}
+	}
+	
+	for (int i = 0; i < 3; i++) {
+		for (int j = 0; j <= i; j++) {
+			sum += a[i][j];
+		}
+	}
+
+	cout << sum;
+	return 0;
+}
