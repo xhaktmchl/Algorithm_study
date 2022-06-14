352157c1b3a74471dc2d4ef3ade89b84b599ea65
xhaktmchl <xhaktmchl@naver.com>
2022-06-14 16:23:22 +0900

    Create [세종대 알고리즘 트레이닝 Level15 08번 O] 다섯 문장 중 가장 긴 문장 찾기.cpp

---
+++ Sejong Algorithm Training/[세종대 알고리즘 트레이닝 Level15 08번 O] 다섯 문장 중 가장 긴 문장 찾기.cpp
@@ -0,0 +1,34 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <algorithm>
+#include <string>
+using namespace std;
+/*
+[세종대 알고리즘 트레이닝 Level15 08번 O] 다섯 문장 중 가장 긴 문장 찾기
+문제:
+접근: 
+풀이: 
+시간복잡도: O()=
+*/
+string str[10];
+int maxL = 0;
+string maxS;
+
+int main() {
+	ios::sync_with_stdio(false); // 계산시간 단축 // cin,scanf 같이 쓰면 오류
+	cin.tie(nullptr); cout.tie(nullptr);// 입출력 시간 단축 // 이것을 쓰면 scanf,printf섞어 쓰면 안됨
+
+	for (int i= 0; i < 5; i++) {
+		cin >> str[i];
+	}
+
+	for (int i = 0; i < 5; i++) {
+		if (maxL < str[i].size()) {
+			maxL = str[i].size();
+			maxS = str[i];
+		}
+	}
+
+	cout << maxS;
+	return 0;
+}
