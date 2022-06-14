96b41ef12f21549c0e5a2aaa226f3ab60521807f
xhaktmchl <xhaktmchl@naver.com>
2022-06-14 11:03:02 +0900

    Create [세종대 알고리즘 트레이닝 Level14 02번 O] 아파트 주민들.cpp

---
+++ Sejong Algorithm Training/[세종대 알고리즘 트레이닝 Level14 02번 O] 아파트 주민들.cpp
@@ -0,0 +1,32 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <algorithm>
+using namespace std;
+/*
+[세종대 알고리즘 트레이닝 Level14 02번 O] 아파트 주민들
+문제:
+접근: 
+풀이: 
+시간복잡도: O()=
+*/
+int a[10][10];
+
+int main() {
+	ios::sync_with_stdio(false); // 계산시간 단축 // cin,scanf 같이 쓰면 오류
+	cin.tie(nullptr); cout.tie(nullptr);// 입출력 시간 단축 // 이것을 쓰면 scanf,printf섞어 쓰면 안됨
+
+	for (int i = 0; i < 5; i++) {
+		for (int j = 0; j < 4; j++) {
+			cin >> a[i][j];
+		}
+	}
+
+	for (int i = 0; i < 5; i++) {
+		int sum = 0;
+		for (int j = 0; j < 4; j++) {
+			sum += a[i][j];
+		}
+		cout << sum << " ";
+	}
+	return 0;
+}
