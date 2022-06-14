008fb1f8601128338398717b4af171dac78e51fa
xhaktmchl <xhaktmchl@naver.com>
2022-06-14 14:50:52 +0900

    Create [세종대 알고리즘 트레이닝 Level14.5 03번 O] 이중 while 연습하기.cpp

---
+++ Sejong Algorithm Training/[세종대 알고리즘 트레이닝 Level14.5 03번 O] 이중 while 연습하기.cpp
@@ -0,0 +1,32 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <algorithm>
+#include <string>
+using namespace std;
+/*
+[세종대 알고리즘 트레이닝 Level14.5 03번 O] 이중 while 연습하기
+문제:
+접근: 
+풀이: 
+시간복잡도: O()=
+*/
+int n;
+
+
+int main() {
+	ios::sync_with_stdio(false); // 계산시간 단축 // cin,scanf 같이 쓰면 오류
+	cin.tie(nullptr); cout.tie(nullptr);// 입출력 시간 단축 // 이것을 쓰면 scanf,printf섞어 쓰면 안됨
+
+	cin >> n;
+	int r =3;
+	while (r > 0) {
+		int c = 5;
+		while (c > 0) {
+			cout << n;
+			c--;
+		}
+		cout << '\n';
+		r--;
+	}
+	return 0;
+}
