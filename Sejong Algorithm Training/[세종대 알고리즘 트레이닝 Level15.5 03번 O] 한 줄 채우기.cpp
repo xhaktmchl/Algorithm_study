0bd82982ff3accec7e0e4ce5cc94eb7ea6801363
xhaktmchl <xhaktmchl@naver.com>
2022-06-14 20:57:06 +0900

    Create [세종대 알고리즘 트레이닝 Level15.5 03번 O] 한 줄 채우기.cpp

---
+++ Sejong Algorithm Training/[세종대 알고리즘 트레이닝 Level15.5 03번 O] 한 줄 채우기.cpp
@@ -0,0 +1,38 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <algorithm>
+#include <string>
+using namespace std;
+/*
+[세종대 알고리즘 트레이닝 Level15.5 03번 O] 한 줄 채우기
+문제:
+접근: 
+풀이: 
+시간복잡도: O()=
+*/
+int a[5][5];
+int r;
+
+int main() {
+	ios::sync_with_stdio(false); // 계산시간 단축 // cin,scanf 같이 쓰면 오류
+	cin.tie(nullptr); cout.tie(nullptr);// 입출력 시간 단축 // 이것을 쓰면 scanf,printf섞어 쓰면 안됨
+
+	int n = 1;
+	for (int i = 2; i >= 0; i--) {
+		for (int j = 3; j >= 0; j--) {
+			a[i][j] = n++;
+		}
+	}
+
+	cin >> r;
+	for (int i = 0; i < 4; i++) {
+		a[r - 1][i] = 7;
+	}
+
+	for (int i = 0; i < 3; i++) {
+		for (int j = 0; j < 4; j++) {
+			cout << a[i][j] << " ";
+		}cout << '\n';
+	}
+	return 0;
+}
