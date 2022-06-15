379a9de760592b8faaf2af1ec6c39fcd04c5734b
xhaktmchl <xhaktmchl@naver.com>
2022-06-15 15:07:22 +0900

    Create [세종대 알고리즘 트레이닝 Level13 08번 O] 두 배열이 완전히 똑같은지 비교하기.cpp

---
+++ Sejong Algorithm Training/[세종대 알고리즘 트레이닝 Level13 08번 O] 두 배열이 완전히 똑같은지 비교하기.cpp
@@ -0,0 +1,34 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+/*
+[세종대 알고리즘 트레이닝 Level13 08번 O] 두 배열이 완전히 똑같은지 비교하기
+문제:
+접근: 
+풀이: 
+시간복잡도: O()=
+*/
+void CompareGo(vector<int> v,vector<int> a) {
+	if (v == a) {
+		cout << "두배열은완전같음";
+	}
+	else { cout << "두배열은같지않음"; }
+}
+
+int main() {
+	ios::sync_with_stdio(false); // 계산시간 단축 // cin,scanf 같이 쓰면 오류
+	cin.tie(nullptr); cout.tie(nullptr);// 입출력 시간 단축 // 이것을 쓰면 scanf,printf섞어 쓰면 안됨
+
+	vector<int> a = { 3,5,1,2,7 };
+	vector<int> v;
+	for (int i = 0; i < 5; i++) {
+		int num;
+		cin >> num;
+		v.push_back(num);
+	}
+
+	CompareGo(v, a);
+	return 0;
+}
