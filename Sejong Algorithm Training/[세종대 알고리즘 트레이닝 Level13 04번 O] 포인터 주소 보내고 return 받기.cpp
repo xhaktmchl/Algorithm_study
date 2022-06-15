9978a209cb4d1466333b7311ce9a2ede5dd733f7
xhaktmchl <xhaktmchl@naver.com>
2022-06-15 12:15:11 +0900

    Create [세종대 알고리즘 트레이닝 Level13 04번 O] 포인터 주소 보내고 return 받기.cpp

---
+++ Sejong Algorithm Training/[세종대 알고리즘 트레이닝 Level13 04번 O] 포인터 주소 보내고 return 받기.cpp
@@ -0,0 +1,29 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <algorithm>
+#include <string>
+using namespace std;
+/*
+[세종대 알고리즘 트레이닝 Level13 04번 O] 포인터 주소 보내고 return 받기
+문제:
+접근: 
+풀이: 
+시간복잡도: O()=
+*/
+
+void ABC(int a, int b, int* sum, int* gop) {
+	*sum = a + b;
+	*gop = a * b;
+}
+int main() {
+	ios::sync_with_stdio(false); // 계산시간 단축 // cin,scanf 같이 쓰면 오류
+	cin.tie(nullptr); cout.tie(nullptr);// 입출력 시간 단축 // 이것을 쓰면 scanf,printf섞어 쓰면 안됨
+
+	int a, b;
+	int sum, gop;
+	cin >> a >> b;
+	
+	ABC(a, b, &sum, &gop);
+	cout << sum << " " << gop;
+	return 0;
+}
