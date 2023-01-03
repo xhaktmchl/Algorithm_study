2bed6fdccf3c878b267513677054f8872f17698e
xhaktmchl <xhaktmchl@naver.com>
2023-01-03 11:51:35 +0900

    Update [백준 2167 c++ O] 2차원 배열의 합.cpp

--- BOJ/누적합/[백준 2167 c++ O] 2차원 배열의 합.cpp
+++ BOJ/누적합/[백준 2167 c++ O] 2차원 배열의 합.cpp
@@ -4,9 +4,10 @@ using namespace std;
 /*
 [백준 2167 c++ O] 2차원 배열의 합
 문제:
-접근1: 완탐 -> 시간 초과 
+접근1: 완탐 -> 시간 초과
 접근2: 행 마다 누적합 저장해놓으면 -> 배열의 구간 합 계산 가능
-풀이: 
+시간복잡도: 
+풀이:
     //1.입력
     //배열
     //범위
@@ -14,24 +15,20 @@ using namespace std;
     //3.각 구간의 배열 합 구하기
     //4.출력: 구간 합
 */
-int n, m, k,xs,ys,xe,ye;
+int n, m, k, xs, ys, xe, ye;
 int a[310][310];
 int pSum[310][310];
 
 int calSum() {
-    
+
     //i,j까지 구간합
-    int sum1 = 0, sum2 = 0;
-    /*for (int i = 1; i <= y1; i++) {
-        sum1 += pSum[i][x1] - pSum[i][0];
-    }*/
+    int sum = 0;
     //x,y까지 구간합
     for (int i = ys; i <= ye; i++) {
-        sum2 += pSum[i][xe] - pSum[i][xs - 1];
-        //cout << sum2 << '\n';
+        sum += pSum[i][xe] - pSum[i][xs - 1];
     }
     //두개의 차 + (i,j)원소 합
-    return sum2;
+    return sum;
 }
 
 int main() {
@@ -49,10 +46,10 @@ int main() {
             pSum[i][j] = pSum[i][j - 1] + a[i][j];
         }
     }
-    
+
     //3.각 구간의 배열 합 구하기
     cin >> k;
-    for (int  i = 0; i < k; i++) {
+    for (int i = 0; i < k; i++) {
         cin >> ys >> xs >> ye >> xe;
         //4.출력: 구간 합
         cout << calSum() << '\n';
