0f4b2ae3ebce2c1b1858b48beb7d0215d2ecdda3
xhaktmchl <xhaktmchl@naver.com>
2023-02-26 23:10:18 +0900

    Create [백준 10926 c++ O] ??!.cpp

---
+++ BOJ/수학(Math)/[백준 10926 c++ O] ??!.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <algorithm> // fill_n, min,max, swap
+//#include <map> // 중복 x 
+#include <string> // getline
+//#include <cstring> // memset, strok, strstr
+//#include <vector>
+//#include <queue> // priority_queue<자료형, 구현체, 비교 연산자> (중복허용), 
+//#include <set> // 트리, 중복 x , multiset
+//#include <unordered_set>
+#include <cmath>
+using namespace std; // lower_bound, upper_bound
+/*
+[백준 10926 c++ O] ??!
+문제:
+접근:
+시간복잡도: O()
+풀이: 
+*/
+string id;
+
+int main()
+{
+    ios::sync_with_stdio(false); cin.tie(nullptr); cout.tie(0);
+    cin >> id;
+
+    id += "??!";
+    cout << id;
+    return 0;
+}
